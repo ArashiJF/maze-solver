f820386ba2db127b55f5ff1731d4c16188a16dd1
Arashi <13753619+ArashiJF@users.noreply.github.com>
2024-08-17 18:02:14 -0500

    - Refactor window class - Create Point class - Create Line class - Draw test lines in window

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, height, width):
         self.root = Tk()
         self.root.title("Maze solver")
-        self.root.geometry(f"{height}x{width}")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
 
     def redraw(self):
         self.root.update_idletasks()
@@ -22,9 +21,33 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_a.x,
+            self.point_a.y,
+            self.point_b.x,
+            self.point_b.y,
+            fill=fill_color,
+            width=2
+        )
+
 def main():
-    print("hi")
     win = Window(800, 600)
+    win.draw_line(Line(Point(0,0), Point(600, 800)))
+    win.draw_line(Line(Point(50, 50), Point(100, 50)))
     win.wait_for_close()
 
 main()
